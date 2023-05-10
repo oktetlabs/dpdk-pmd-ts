oktetlabs/dpdk-pmd-ts

commit 8cb5da8a897f88d22bec3ced731095f71b71b9f7

prologue: clean env variable
The env environment variable is used to control resources of a test.
Usually, it is initialised on the start of a test and destroyed on
the end of it.

Thus, update the text so that the clean env operation is called on
the end of the prologue test.

OL-Redmine-Id: 12602
Signed-off-by: Boleslav Stankevich <boleslav.stankevich@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

ts/prologue.c
@@ -648,10 +648,11 @@ main(int argc, char **argv)
 /* Redefine as empty to avoid environment processing here */
 #undef TEST_START_VARS
 #define TEST_START_VARS
+/* Redefine to initialise and clean up the env variable only. */
 #undef TEST_START_SPECIFIC
-#define TEST_START_SPECIFIC
+#define TEST_START_SPECIFIC (void)tapi_env_init(&env)
 #undef TEST_END_SPECIFIC
-#define TEST_END_SPECIFIC
+#define TEST_END_SPECIFIC TEST_END_ENV
 
     te_bool           xdp_detected = FALSE;
     unsigned int      service_core_count;

commit 85d2738fde6652d9de8597d9d4225be3612a61fa

prologue: add RPC servers from local tree to agent tree
It fixes reuse_pco mode. All RPC servers should be created
before the real testing to be able to use reuse_pco mode.

Fixes: 8cb5da8a897f ("prologue: clean env variable")
Signed-off-by: Denis Pryazhennikov <denis.pryazhennikov@arknetworks.am>
Reviewed-by: Viacheslav Galaktionov <viacheslav.galaktionov@arknetworks.am>
Reviewed-by: Artemii Morozov <artemii.morozov@arknetworks.am>

ts/prologue.c
@@ -791,6 +791,12 @@ main(int argc, char **argv)
         TEST_VERDICT("Failed to disable IPv6 and delete IPv4 addresses on "
                      "interfaces mentioned in networks configuration: %r", rc);
 
+    rc = tapi_cfg_rpcs_local_to_agent();
+    if (rc != 0)
+        TEST_FAIL("Failed to start all RPC servers: %r", rc);
+
+    CFG_WAIT_CHANGES;
+
     TEST_START_ENV;
 
     TEST_GET_PCO(iut_rpcs);
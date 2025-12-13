87dc7fdcfb1c2dcd2074cf16a60e2bb9a04152f8
Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
2025-12-13 13:01:16 +0300

    doc: support PDF generation for documentation
    Note, that right now it's hard to read since it includes global
    namespace.
    
    Signed-off-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
    Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>
    Reviewed-by: Viacheslav Galaktionov <viacheslav.galaktionov@arknetworks.am>

--- ts/doc/conf.py
+++ ts/doc/conf.py
@@ -7,6 +7,15 @@ copyright = u'(c) Copyright 2016 - 2022 Xilinx, Inc. All rights reserved.'
 author = u'Andrew Rybchenko'
 version = u'1.0'
 release = u'release'
+latex_documents = [
+    (
+        'index',
+        'dpdk_ethdev_test_suite.tex',
+        u'DPDK EthDev Test Suite',
+        u'Andrew Rybchenko',
+        'manual',
+    ),
+]
 
 # Specify the path to Doxyrest extensions for Sphinx:
 
